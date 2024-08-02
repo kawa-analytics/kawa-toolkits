898cab665368e10a72531f21d6da71766082a15a
emmanuel <emmanuel@kawa-analytics.com>
2024-08-02 14:06:41 -0400

    Improve the rolling average

--- toolkits/basic/sliding_average.py
+++ toolkits/basic/sliding_average.py
@@ -4,9 +4,17 @@ from kywy.client.kawa_decorators import kawa_tool
 
 
 @kawa_tool(
-    inputs={'measure': float, 'date': datetime.date},
+    inputs={
+        'measure': float,
+        'dimension': str,
+        'date': datetime.date,
+    },
     outputs={'sliding_average': float},
 )
 def execute(df: pd.DataFrame) -> pd.DataFrame:
-    df['sliding_average'] = df['measure'].rolling(window=10).mean()
+    df = df.sort_values('date')
+    df['sliding_average'] = (df.groupby('dimension')['measure']
+                             .rolling(window=10)
+                             .mean()
+                             .reset_index(level=0, drop=True))
     return df
