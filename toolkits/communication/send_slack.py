7855b48cfb5c44ef98ab7e619464f97a6ff9ceae
emmanuel <emmanuel@kawa-analytics.com>
2024-08-08 17:18:31 -0400

    add first version for Slack messaging

---
+++ toolkits/communication/send_slack.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from kywy.client.kawa_decorators import kawa_tool
+from slack_sdk import WebClient
+
+
+@kawa_tool(
+    inputs={'dimension': str, 'metric': str, },
+    secrets={'channel': 'slack_channel', 'token': 'slack_token', },
+)
+def execute(df: pd.DataFrame, channel: str, token: str) -> pd.DataFrame:
+    send_to_slack(
+        channel=channel,
+        token=token,
+        message='Hi')
+
+
+def send_to_slack(channel: str, token: str, message: str):
+    client = WebClient(token=token)
+    client.chat_postMessage(
+        channel=channel,
+        text=message
+    )
