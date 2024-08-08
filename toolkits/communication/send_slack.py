5c660bd9b00304b82cd52dd839c31c048fbcdfd6
emmanuel <emmanuel@kawa-analytics.com>
2024-08-08 17:54:57 -0400

    add the data in the message

--- toolkits/communication/send_slack.py
+++ toolkits/communication/send_slack.py
@@ -9,10 +9,11 @@ from slack_sdk import WebClient
     secrets={'channel': 'slack_channel', 'token': 'slack_token', },
 )
 def execute(df: pd.DataFrame, channel: str, token: str) -> pd.DataFrame:
+    table = df.head(100).to_markdown()
     send_to_slack(
         channel=channel,
         token=token,
-        message='Hi')
+        message=f'Here is your data:\n```{table}```')
 
 
 def send_to_slack(channel: str, token: str, message: str):
@@ -21,3 +22,4 @@ def send_to_slack(channel: str, token: str, message: str):
         channel=channel,
         text=message
     )
+
