ed8fd697bad41e5da442a79e3c69c20332fd13fc
emmanuel <emmanuel@kawa-analytics.com>
2024-08-09 11:05:35 -0400

    add logger in gender detection

--- toolkits/ai/gender.py
+++ toolkits/ai/gender.py
@@ -1,6 +1,10 @@
+import logging
+
 import pandas as pd
 from gender_detector.gender_detector import GenderDetector
 from kywy.client.kawa_decorators import kawa_tool
+
+logger = logging.getLogger('script-logger')
 
 
 @kawa_tool(
@@ -10,4 +14,5 @@ from kywy.client.kawa_decorators import kawa_tool
 def execute(df: pd.DataFrame) -> pd.DataFrame:
     detector = GenderDetector('us')
     df['gender'] = df['first_name'].apply(lambda x: detector.guess(x))
+    logger.info(df)
     return df
