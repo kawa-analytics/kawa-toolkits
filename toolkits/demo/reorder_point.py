31b6fdd60d35a538ce2b3fb05ac0d077f1ac0c71
greghass <greghass@gmail.com>
2024-09-12 16:13:33 +0200

    Update reorder_point.py

--- toolkits/demo/reorder_point.py
+++ toolkits/demo/reorder_point.py
@@ -6,12 +6,24 @@ logger = logging.getLogger('script-logger')
 
 
 @kawa_tool(
-    inputs={'Units Sold': float, 'total_days': float, 'product_leadtime_mapping': float},
+    inputs={'units_sold': float, 'transaction_date': date, 'product_leadtime': float, 'product_name': str},
     outputs={'Reorder Point': float}
 )
 def compute_reorder_point(df):
     # Define the fixed safety stock
     safety_stock = 10
-    df['Average Daily Usage'] = df['Units Sold'] / df['total_days']
-    df['Reorder Point'] = (df['Average Daily Usage'] * df['product_leadtime_mapping']) + safety_stock
+    
+    aggregated = df.groupby(by=['product_name']).agg(
+    min_date=('transaction_date', 'min'),
+    max_date=('transaction_date', 'max'),
+    total_sold=('units_sold', 'sum')).reset_index()
+
+    # Merge the original DataFrame with the aggregated DataFrame
+    df_merged = pd.merge(df, aggregated, on='product_name', how='left')
+
+    # Compute the total days between min and max transaction date per product
+    df_merged['total_days'] = df_merged.apply(lambda row: (row['max_date'] - row['min_date']).days, axis=1)
+    
+    df['Average Daily Usage'] = df['total_sold'] / df['total_days']
+    df['Reorder Point'] = (df['Average Daily Usage'] * df['product_leadtime']) + safety_stock
     return df
