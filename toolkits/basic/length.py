9fb8b782f01621862a5449da0bb9b09d2669ac4e
Emmanuel Wiesenfeld <96352851+wiesenfeK@users.noreply.github.com>
2024-11-29 10:28:27 -0500

    Update length.py

--- toolkits/basic/length.py
+++ toolkits/basic/length.py
@@ -7,11 +7,12 @@ logger = logging.getLogger('script-logger')
 
 @kawa_tool(
     inputs={'text': str},
-    outputs={'length': float, 'length3': float},
+    outputs={'length': float,'length2': float, 'length3': float},
 )
 def main(df: pd.DataFrame) -> pd.DataFrame:
     logger.info('Starting the execution now')
     logger.info(df)
     df['length'] = df['text'].apply(lambda x: len(x))
+    df['length2'] = df['text'].apply(lambda x: len(x))
     df['length3'] = df['text'].apply(lambda x: len(x))
     return df
