f3332460a3333aed9f9cf3844780f9fb80c58958
Emmanuel Wiesenfeld <96352851+wiesenfeK@users.noreply.github.com>
2024-11-29 10:16:17 -0500

    Update length.py

--- toolkits/basic/length.py
+++ toolkits/basic/length.py
@@ -7,8 +7,7 @@ logger = logging.getLogger('script-logger')
 
 @kawa_tool(
     inputs={'text': str},
-    outputs={'length': float},
-    outputs={'length2': float},
+    outputs={'length': float, 'length2': float},
 )
 def main(df: pd.DataFrame) -> pd.DataFrame:
     logger.info('Starting the execution now')
