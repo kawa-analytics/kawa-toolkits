b74d21ce16da6c43e7c8845d7b76f8adb92443ff
emmanuel <emmanuel@kawa-analytics.com>
2025-07-21 10:19:52 -0400

    demo

--- toolkits/basic/sliding_average.py
+++ toolkits/basic/sliding_average.py
@@ -5,17 +5,14 @@ from kywy.client.kawa_decorators import kawa_tool
 
 
 @kawa_tool(
-    inputs={
-        'measure': float,
-        'dimension': str,
-        'date': datetime.date,
-    },
+    inputs={'measure': float,'dimension': str,'date': datetime.date},
     outputs={'sliding_average': float},
+    parameters={'window':{'type':float, 'default':10}}
 )
-def execute(df: pd.DataFrame) -> pd.DataFrame:
+def execute(df: pd.DataFrame, window) -> pd.DataFrame:
     df = df.sort_values(['dimension', 'date'])
     df['sliding_average'] = (df.groupby('dimension')['measure']
-                             .rolling(window=20)
+                             .rolling(window=window)
                              .mean()
                              .reset_index(level=0, drop=True))
     return df
